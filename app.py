5d22c9b91327c88f7ea8af3d9b313c36ee54b922
Gwt-space <ajaymgwt@gmail.com>
2025-06-05 12:40:46 +0530

    updated the flask

--- app.py
+++ app.py
@@ -137,6 +137,6 @@ def send_whatsapp_message(to_number, lead_data):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
 
